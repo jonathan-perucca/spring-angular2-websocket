295eba68dd5f248cef628685b73ba8338e3cd907
jperucca <under.64@gmail.com>
2016-03-27 15:00:10 +0200

    [Angular] > simply displaying initial values (mock ones)

--- src/main/resources/static/websocket/messager.ts
+++ src/main/resources/static/websocket/messager.ts
@@ -1,15 +1,20 @@
 import {Component} from 'angular2/core';
+import {Message} from './message';
 
 @Component({
     selector: 'websocket-messager',
-    templateUrl: 'app/view.html'
+    template: `
+    <ul>
+      <li *ngFor="#message of messages">
+        {{message.content}}
+      </li>
+    </ul>
+    `
 })
 export class WebsocketMessager {
     messages: Message[] = [
-        {content: 'Awesome communication'},
-        {content: 'Seriously ? Angular 2 + WS over Stomp ?'},
-        {content: 'Dockerize all of it buddy'},
+        {content : 'Awesome communication'},
+        {content : 'Seriously ? Angular 2 + WS over Stomp ?'},
+        {content : 'Dockerize all of it buddy'}
     ];
-
-
 }
